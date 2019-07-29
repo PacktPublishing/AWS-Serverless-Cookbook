3083a9748e4ac0e606967a489e67d3141df2afaa
Riyaz Sayyad <riyaz@riyaz.net>
2019-07-29 20:47:59 +0530

    updated chapter 6

--- Chapter06/sls/hello-serverless/add.js
+++ Chapter06/sls/hello-serverless/add.js
@@ -12,7 +12,4 @@ module.exports.add = async (event, context) => {
         statusCode: 200,
         body: JSON.stringify(output)
     };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
